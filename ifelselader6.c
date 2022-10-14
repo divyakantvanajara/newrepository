45f6409a5af3e7e17c668b4f5f4deaaff56a3e78
divyakantvanajara <divyakant.1452@gmail.com>
2022-10-14 17:55:33 +0530

    new

--- ifelselader6.c
+++ ifelselader6.c
@@ -17,47 +17,47 @@ void main()
     }
     else if( (month==4 && days>=20 && days<=31 ) || ( month==5 && days>=1 && days<=20))
     {
-        printf("your zodic sign is taurus and symbol is the ram");
+        printf("your zodic sign is taurus and symbol is the bull");
     }
     else if( (month==5 && days>=21 && days<=31 ) || ( month==6 && days>=1 && days<=20))
     {
-        printf("your zodic sign is gemini and symbol is the ram");
+        printf("your zodic sign is gemini and symbol is the twins");
     }
     else if( (month==6 && days>=21 && days<=31 ) || ( month==7 && days>=1 && days<=22))
     {
-        printf("your zodic sign is cancer and symbol is the ram");
+        printf("your zodic sign is cancer and symbol is the crab");
     }
     else if( (month==7 && days>=23 && days<=31 ) || ( month==8 && days>=1 && days<=22))
     {
-        printf("your zodic sign is leo and symbol is the ram");
+        printf("your zodic sign is leo and symbol is the lion");
     }
     else if( (month==8 && days>=23 && days<=31 ) || ( month==9 && days>=1 && days<=22))
     {
-        printf("your zodic sign is virgo and symbol is the ram");
+        printf("your zodic sign is virgo and symbol is the virgin");
     }
      else if( (month==9 && days>=23 && days<=31 ) || ( month==10 && days>=1 && days<=22))
     {
-        printf("your zodic sign is libra and symbol is the ram");
+        printf("your zodic sign is libra and symbol is the scales");
     }
      else if( (month==10 && days>=23 && days<=31 ) || ( month==11 && days>=1 && days<=22))
     {
-        printf("your zodic sign is scorpio and symbol is the ram");
+        printf("your zodic sign is scorpio and symbol is the scorpion");
     }
      else if( (month==11 && days>=23 && days<=31 ) || ( month==12 && days>=1 && days<=22))
     {
-        printf("your zodic sign is sagittarius and symbol is the ram");
+        printf("your zodic sign is sagittarius and symbol is the archer");
     }
      else if( (month==12 && days>=23 && days<=31 ) || ( month==1 && days>=1 && days<=19))
     {
-        printf("your zodic sign is capricorn and symbol is the ram");
+        printf("your zodic sign is capricorn and symbol is the goat");
     }
      else if( (month==1 && days>=20&& days<=31 ) || ( month==2 && days>=1 && days<=17))
     {
-        printf("your zodic sign is aquarius and symbol is the ram");
+        printf("your zodic sign is aquarius and symbol is the water bearer");
     }
      else if( (month==2 && days>=18 && days<=29 ) || ( month==3 && days>=1 && days<=19))
     {
-        printf("your zodic sign is pisces and symbol is the ram");
+        printf("your zodic sign is pisces and symbol is the fishes");
     }
     else
     {
